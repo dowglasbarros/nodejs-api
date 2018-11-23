ae5c8ae31b9e7044bb275c6efd5c4ac44353fc6f
Dowglas <dowglas_rodrigo@hotmail.com>
2018-11-23 11:22:32 -0200

    app.js settings

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const bodyParser = require("body-parser");
-// const typeorm = require("typeorm");
 
+// express
 const express = require("express");
 const app = express();
-app.use(bodyParser.json());
 const router = express.Router();
-
-const connection = require("./database/databaseProducts");
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
 
 // routes
 const index = require('./routes/index');
 const productsRoute = require('./routes/productsRoute');
 
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+app.use(bodyParser.json());
 app.use('/', index);
 app.use('/products', productsRoute);
 
